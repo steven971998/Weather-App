4c9847199d4b8363483f35ddcef2fbec4f3ed740
Steven Fernandes <steven971998@gmail.com>
2022-04-05 16:34:59 +0530

    Weather App 1.0

---
+++ src/components/temp.js
@@ -0,0 +1,58 @@
+// https://api.openweathermap.org/data/2.5/weather?q=pune&appid=ec818d0159144c731f41ff1c1ee2cdb2
+
+import React, {useState,useEffect} from 'react'
+import './style.css'
+import Weathercard from './weathercard'
+
+
+const Temp = () => {
+
+const [ searchValue, setSearchValue] = useState("airoli")
+const [tempInfo,setTempInfo] =useState({})
+
+const getWeatherInfo = async () => { 
+try {
+    let url= `https://api.openweathermap.org/data/2.5/weather?q=${searchValue}&units=metric&appid=ec818d0159144c731f41ff1c1ee2cdb2`
+
+const res = await fetch(url)
+const data = await res.json(); //converting the data into readable format.
+console.log(data)
+
+const {temp,humidity,pressure}=data.main; //our temperature and some other data is present inside main. 
+const {main:weathermood}=data.weather[0]; //this data is present inside an array so as
+const {name} = data ;
+const {speed} = data.wind;
+const {country,sunset}=data.sys;
+
+const myNewWeatherInfo = {
+    temp,humidity,pressure,weathermood,name,speed,country,sunset
+};
+setTempInfo(myNewWeatherInfo);
+} catch (error) {
+    console.log(error)
+}
+};
+
+useEffect(() => {
+    getWeatherInfo(); //weather app should open directly as default when we reload it and it should give data of our place.
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+}, []) 
+
+
+    return (
+        <>
+            <div className="wrap">
+                <div className="search">
+                    <input type="search" placeholder='search...' autoFocus id="search" className='searchTerm' value={searchValue} onChange={(e)=> setSearchValue(e.target.value)}  />
+                    <button className="searchButton" type='button' onClick={getWeatherInfo}>Search</button>
+                </div>
+            </div>
+
+
+            {/* our temp card */}
+            <Weathercard  tempInfo={tempInfo}/>   {/* we are passing temp info as a props. */}
+        </>
+    )
+}
+
+export default Temp
---
+++ src/components/weathercard.js
@@ -0,0 +1,89 @@
+import React, { useState, useEffect } from 'react'
+
+const Weathercard = ({tempInfo}) => {
+
+    const [weatherState,setWeatherState]=useState("")
+
+const {temp,humidity,pressure,weathermood,name,speed,country,sunset} = tempInfo; //destructuring.
+
+useEffect(() => {
+  if (weathermood){
+      switch (weathermood) {
+          case "Clouds":setWeatherState("wi-day-cloudy");
+              break;
+          case "Haze":setWeatherState("wi-fog");
+              break;
+          case "Clear":setWeatherState("wi-day-sunny");
+              break;
+          case "Mist":setWeatherState("wi-dust");
+              break;
+          case "Rain":setWeatherState("wi-rain");
+              break;
+          case "Thunderstorm":setWeatherState("wi-thunderstorm");
+              break;
+          default:setWeatherState("wi-day-sunny");
+              break;
+      }
+  }
+}, [weathermood])
+
+
+//converting seconds into time
+let sec=sunset;
+let date= new Date(sec* 1000); //we'll get milliseconds.
+let timeStr=`${date.getHours()}:${date.getMinutes()}`
+
+  return (
+    <>
+    
+    <article className="widget"> {/* //it will create a box */}
+                <div className="weatherIcon">
+                    <i className={`wi ${weatherState}`}></i>
+                </div>
+                <div className="weatherInfo">
+                    <div className="temperature">
+                        <span>{temp}°C</span>
+                    </div>
+                    <div className="description">
+                        <div className="weatherCondition">
+                            {weathermood}
+                        </div>
+                        <div className="place">
+                            {name}, {country}
+                        </div>
+                    </div>
+                </div>
+                <div className="date">{new Date().toLocaleString()}</div>
+                {/* our 4 coloumn section  */}
+                <div className="extra-temp">
+                    <div className="temp-info-minmax">
+                        <div className="two-sided-section">
+                            <p><i className={'wi wi-sunset'}></i></p>
+                            <p className='extra-info-leftside'>{timeStr} <br/>Sunset</p>
+                        </div>
+
+                        <div className="two-sided-section">
+                            <p><i className={'wi wi-humidity'}></i></p>
+                            <p className='extra-info-leftside'>{humidity} %<br/>Humidity</p>
+                        </div>
+                    </div>
+
+
+                    <div className="weather-extra-info">
+                    <div className="two-sided-section">
+                            <p><i className={'wi wi-rain'}></i></p>
+                            <p className='extra-info-leftside'>{pressure} hpa <br/> Pressure</p>
+                        </div>
+
+                        <div className="two-sided-section">
+                            <p><i className={'wi wi-strong-wind'}></i></p>
+                            <p className='extra-info-leftside'>{speed} m/s<br/>Speed</p>
+                        </div>
+                    </div>
+                </div>
+            </article>
+    </>
+  )
+}
+
+export default Weathercard
